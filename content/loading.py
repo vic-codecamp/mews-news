c04c639e880fe1053d24d086f7fb81ef297225c3
Andres <contact@andresbott>
2019-11-02 15:56:23 +0100

    working

--- content/loading.py
+++ content/loading.py
@@ -8,7 +8,7 @@ from sklearn.naive_bayes import MultinomialNB
 
 df = pd.read_csv('history.csv')
 
-features = pd.DataFrame(df, columns=['title'])
+features = pd.DataFrame(df, columns=['title',"action"])
 
 
 
@@ -16,40 +16,42 @@ features = pd.DataFrame(df, columns=['title'])
 
 class NewsTittles:
 
-    labels = [0, 1, 2,3,4,5,6,7,8,9]
+    labels = [0, 1, 2]
     label_cat = ["down", "netruaal", "up"]
 
     def load(self,data):
 
         documents =[]
+        learn_targets = []
 
         # iterate over the rows and append every row to the documents array
         for index, row in data.iterrows():
             documents.append(row['title'])
+            learn_targets.append(row["action"])
 
         # count vectorizer of the documents
         count_vect = CountVectorizer()
         X_train_counts = count_vect.fit_transform(documents)
-        print(X_train_counts)
+        # print(X_train_counts)
 
         # Term Freq
         tf_transformer = TfidfTransformer(use_idf=False).fit(X_train_counts)
         X_train_tf = tf_transformer.transform(X_train_counts)
-        print(X_train_tf.shape)
+        # print(X_train_tf.shape)
+
 
         # Term Frequency times Inverse Document Frequency
         tfidf_transformer = TfidfTransformer()
         X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
-        print(X_train_tfidf.shape)
+        # print(X_train_tfidf)
 
 
 
-        clf = MultinomialNB().fit(X_train_tfidf, self.labels)
+        clf = MultinomialNB().fit(X_train_tfidf, learn_targets)
 
 
         docs_new = [
-            'A disgraced Tor developer has resurfaced to testify against one of his accusers',
-            'Sign Up for Your Free Year of Apple TV+ Right Now'
+            'Bitcoin’s Defense of Major Support May Fuel Price Bounce to $9,600',
         ]
 
         X_new_counts = count_vect.transform(docs_new)
@@ -58,10 +60,8 @@ class NewsTittles:
         predicted = clf.predict(X_new_tfidf)
 
         for doc, category in zip(docs_new, predicted):
-            print(doc)
-            print(category)
-            # print('%r => %s' % (doc, self.label_cat[category]))
-            # print('%r => %s' % (doc, self.label_cat[category]))
+            print('%r => %s' % (doc, self.label_cat[category]))
+
 
 
 

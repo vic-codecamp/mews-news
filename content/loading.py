bebb3f4f822e9bac6848d7047c786720cea8f6dc
vs-slavchev <vs_slavchev2@abv.bg>
2019-11-02 13:13:20 +0100

    Add csv format

--- content/loading.py
+++ content/loading.py
@@ -6,4 +6,4 @@ url = ('https://newsapi.org/v2/top-headlines?'
        'country=us&'
        'apiKey=aeba79676d064e08a9c687c0d988115f')
 response = requests.get(url)
-print response.json()
+print(response.json())
